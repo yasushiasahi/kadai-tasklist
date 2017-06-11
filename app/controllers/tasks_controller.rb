84b20152dc5d6e253460b729da09ff06febb18d7
yasushiasahi <asahi1600@gmail.com>
2017-06-11 15:13:25 +0000

    DRYとバリテーション

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,60 +1,56 @@
 class TasksController < ApplicationController
+  before_action :set_selected_id,only:[:edit,:show,:update,:destroy]
+  
   def index
     @all_tasks = Task.all
   end
   
   def create
     new_task = Task.new(params_task)
-    
-    if new_task.save
+    result = new_task.save
+    move_to_index_with_display_flash(result)
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def edit
+  end
+  
+  def show
+  end
+  
+  def update
+    result = @task.update(params_task)
+    move_to_index_with_display_flash(result)
+  end
+  
+  def destroy
+    result = @task.destroy
+    move_to_index_with_display_flash(result)
+  end
+  
+  #============================================#
+  private
+  
+  def move_to_index_with_display_flash(result)
+    if result
       flash[:success] = "タスクが正常に作成されました"
       redirect_to Task
     else
       flash[:success] = "タスクの作成に失敗しました"
-      render task
+      render tasks
     end
   end
   
-  def new
-    @new_task = Task.new
+  def set_selected_id
+    @task = Task.find(params[:id])
   end
-  
-  def edit
-    @edit_task = Task.find(params[:id])
-  end
-  
-  def show
-    @show_task = Task.find(params[:id])
-  end
-  
-  def update
-    update_task = Task.find(params[:id])
-    
-    if update_task.update(params_task)
-      flash[:success] = "タスクが正常に更新されました"
-      redirect_to Task
-    else
-      flash[:success] = "タスクの更新に失敗しました"
-      render task
-    end
-  end
-  
-  def destroy
-    @destroy_task = Task.find(params[:id])
-    
-    if @destroy_task.destroy
-      flash[:success] = "タスクが正常に削除されました"
-      redirect_to Task
-    else
-      flash[:success] = "タスクの削除に失敗しました"
-      render task
-    end
-  end
-  
-  private
   
   def params_task
     params.require(:task).permit(:content)
   end
+  #============================================#
   
 end
