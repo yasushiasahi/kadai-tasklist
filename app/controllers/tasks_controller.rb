149e6b9fca71077006484bab88cceeeae5c43304
yasushiasahi <asahi1600@gmail.com>
2017-06-12 11:41:59 +0000

    bootstrapを適用

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,9 +6,9 @@ class TasksController < ApplicationController
   end
   
   def create
-    new_task = Task.new(params_task)
-    result = new_task.save
-    move_to_index_with_display_flash(result)
+    @task = Task.new(params_task)
+    result = @task.save
+    move_to_index_with_display_flash(result,:new,"作成")
   end
   
   def new
@@ -23,24 +23,24 @@ class TasksController < ApplicationController
   
   def update
     result = @task.update(params_task)
-    move_to_index_with_display_flash(result)
+    move_to_index_with_display_flash(result,:edit,"変更")
   end
   
   def destroy
     result = @task.destroy
-    move_to_index_with_display_flash(result)
+    move_to_index_with_display_flash(result,:show,"削除")
   end
   
   #============================================#
   private
   
-  def move_to_index_with_display_flash(result)
+  def move_to_index_with_display_flash(result,where_you_were,cud)
     if result
-      flash[:success] = "タスクが正常に作成されました"
-      redirect_to Task
+      flash[:success] = "タスクが正常に" + cud + "されました"
+      redirect_to "/"
     else
-      flash[:success] = "タスクの作成に失敗しました"
-      render tasks
+      flash[:danger] = "タスクの" + cud + "に失敗しました"
+      render where_you_were
     end
   end
   
